a217b02aa995e2ee83cde68eb37423ec5b34383c
Tom Hayden <tomhaydenss@gmail.com>
2019-08-06 00:35:37 -0300

    adding ensemble filter to member's index view

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,6 @@
 class Member < ApplicationRecord
+  include Filterable
+  
   belongs_to :membership, optional: true
   belongs_to :ensemble, optional: true
   has_many :phones, dependent: :delete_all
@@ -7,5 +9,12 @@ class Member < ApplicationRecord
   has_many :leaders
   has_many :leader_roles, through: :leaders
 
+  scope :ensemble_id, ->(ensemble_id) { where('ensemble_id = ?', ensemble_id) }
+
   accepts_nested_attributes_for :identity_documents, :phones, :addresses, :reject_if => :all_blank, :allow_destroy => true
+
+  def highest_ensemble_level_through_leadership
+    leaders.select { |leader| leader.ensemble.ensemble_level.precedence_order.present? }
+          .sort_by { |leader| leader.ensemble.ensemble_level.precedence_order }.first.ensemble
+  end
 end
