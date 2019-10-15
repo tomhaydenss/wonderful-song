f5ae3a183d87605f713e071dc982ca33e5b5500e
Tom Hayden <tomhaydenss@gmail.com>
2019-10-14 21:07:10 -0300

    improving validation for the following identity documents: id, tax payer id

--- app/models/identity_document.rb
+++ app/models/identity_document.rb
@@ -2,10 +2,31 @@ class IdentityDocument < ApplicationRecord
   belongs_to :identity_document_type
 
   validates :number, presence: true
-  validates :number, format: { with: /\A[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}\z/ }, if: :tax_payer_type?
   validates :number, uniqueness: { scope: :member_id }
+  validates :number, format: { with: /\A[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}\z/ }, if: :tax_payer_type?
+  validate :validate_tax_payer_number, if: :tax_payer_type?
+  validate :validate_id_number, if: :id_type?
+  validates :complement, presence: true, if: :id_type?
 
   def tax_payer_type?
     identity_document_type.tax_payer?
+  end
+
+  def id_type?
+    identity_document_type.id?
+  end
+
+  private
+
+  def digits_only(number)
+    number.scan(/\d/).join('')
+  end
+
+  def validate_tax_payer_number(validator = CPFValidator)
+    errors.add(:number, :invalid) unless validator.cpf_valid?(digits_only(self.number))
+  end
+  
+  def validate_id_number
+    errors.add(:number, :invalid) unless digits_only(self.number).length >= 4
   end
 end
