6b99e04f9df8ce109452a4af3c1afad138bbf335
Tom Hayden <tomhaydenss@gmail.com>
2020-02-25 00:39:49 -0300

    Fixing email config at prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,10 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_options = { from: ENV['DEFAULT_SENDER'] || 'from@example.com' }
+
+  config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] || 'your.domain.com' }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
